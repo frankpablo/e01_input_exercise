d586cff577369f63894aa3ea6a8f4fec83fc3180
Pablo Frank <frankpablo@gmail.com>
2025-01-24 17:03:05 -0500

    Created other.py

---
+++ other.py
@@ -0,0 +1,12 @@
+# ------------------------------------------------------
+# INFO: try to run this file from the Terminal  
+# ------------------------------------------------------
+total_pets = 0
+
+a = int ( input ("gimme # doggos: ") )
+
+b = int ( input ("gimme # cattos: ") )
+
+total_pets += (a + b)
+
+print("total_pets: ", total_pets)
